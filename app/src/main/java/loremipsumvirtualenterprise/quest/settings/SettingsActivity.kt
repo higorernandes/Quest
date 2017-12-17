7f601dc21ce09034ca765ccff2edc1299168f604
Higor Ernandes <higorernandes@gmail.com>
2017-12-17 21:41:48 -0200

    Alteração no fluxo de login/logout.

--- app/src/main/java/loremipsumvirtualenterprise/quest/settings/SettingsActivity.kt
+++ app/src/main/java/loremipsumvirtualenterprise/quest/settings/SettingsActivity.kt
@@ -2,14 +2,18 @@ package loremipsumvirtualenterprise.quest.settings
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.Toolbar
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_settings.*
 import loremipsumvirtualenterprise.quest.R
 import loremipsumvirtualenterprise.quest.account.LoginActivity
 import loremipsumvirtualenterprise.quest.generic.GenericWebViewActivity
 import loremipsumvirtualenterprise.quest.generic.QuestGenericActivity
+import loremipsumvirtualenterprise.quest.main.MainActivity
 import loremipsumvirtualenterprise.quest.util.SharedPreferencesHelper
 
 class SettingsActivity : QuestGenericActivity()
@@ -50,16 +54,29 @@ class SettingsActivity : QuestGenericActivity()
         settingsChangePasswordLinearLayout.setOnClickListener { startActivity(ChangePasswordActivity.getActivityIntent(this)) }
         settingsPrivacyPolicyLinearLayout.setOnClickListener { startActivity(GenericWebViewActivity.getActivityIntent(this, "https://www.google.com.br/intl/en_br/policies/privacy/?fg=1", GenericWebViewActivity.WebViewType.WEB_VIEW_PRIVACY_POLICY))}
         settingsAboutLinearLayout.setOnClickListener { startActivity(GenericWebViewActivity.getActivityIntent(this, "https://www.google.com.br/intl/en_br/about/?utm_source=google.com&utm_medium=referral&utm_campaign=hp-footer&fg=1", GenericWebViewActivity.WebViewType.WEB_VIEW_ABOUT))}
-        settingsLogoutLinearLayout.setOnClickListener { doLogout() }
+        settingsLogoutLinearLayout.setOnClickListener { confirmLogout() }
     }
 
     // Actions
+    private fun confirmLogout() {
+        val alertDialog: AlertDialog = AlertDialog.Builder(this).setMessage(resources.getString(R.string.logout_confirmation)).create()
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, resources.getString(R.string.generic_yes)) { _, _ -> doLogout() }
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, resources.getString(R.string.generic_no)) { dialog, _ -> dialog.dismiss() }
+
+        val text = alertDialog.window.findViewById<TextView>(android.R.id.message)
+        val button1 = alertDialog.findViewById<TextView>(android.R.id.button1)
+        alertDialog.show()
+
+        text?.typeface = QuestGenericActivity.sAvenirNextRegular
+        button1?.typeface = QuestGenericActivity.sAvenirNextRegular
+    }
+
     private fun doLogout() {
         firebaseAuth!!.signOut()
         val loginActivityIntent : Intent = LoginActivity.getActivityIntent(this)
-        loginActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        startActivity(loginActivityIntent)
+        MainActivity.instance?.finish()
         finish()
+        startActivity(loginActivityIntent)
         SharedPreferencesHelper.removeFromSharedPreferences(this, SharedPreferencesHelper.USER_LOGGED)
     }
 
